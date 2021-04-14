0ed32219326d353f58ee7b677f34d77ddc077b53
nwnordahl <nicolaiwnordahl@gmail.com>
2021-04-14 12:09:57 +0200

    get single product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require("../models/productModel");
 
+// @desc  Gets all products
+// @route GET /api/products
 async function getProducts(req, res) {
   try {
     const products = await Product.findAll();
@@ -10,6 +12,25 @@ async function getProducts(req, res) {
   }
 }
 
+// @desc  Gets single product
+// @route GET /api/products/:id
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
   getProducts,
+  getProduct,
 };
